825454eb99ff4e75d7daca5da403f98a8868c6e0
Ezequiel <ezetl91@gmail.com>
2015-04-15 17:30:44 -0300

    primera version de kernel separable andando

--- Practico2/HarrisCorners.cpp
+++ Practico2/HarrisCorners.cpp
@@ -3,45 +3,15 @@
 #include <vector>
 #include <cassert>
 
-#define SIGMA 2.0
+#define SIGMA 9.0
+#define SIGMA1 2.0
 #define K_ 0.04
-#define THRESHOLD 2000
+#define THRESHOLD 100.0
 
 typedef unsigned char Byte;
 
+
 void gradientSobel(cv::Mat& img, cv::Mat& img_x, cv::Mat& img_y) {
-  // gradiente en X
-  assert((img.rows == img_x.rows) && (img.cols == img_x.cols) && \
-         (img.cols == img_y.cols) && (img.cols == img_y.cols));
-
-  // EJERCICIO 1:
-  // - implementar "zero padding" en el c贸mputo del gradiente
-  // - analizar correlaci贸n vs. convoluci贸n
-
-  // EJERCICIO 3
-  // - c贸mputo usando m谩scaras separables
-  // * implementar como kernel en GPU
-
-  for (int i = 1; i < img.rows - 1; ++i) {
-    float* img_0 = img.ptr<float>(i - 1);
-    float* img_1 = img.ptr<float>(i);
-    float* img_2 = img.ptr<float>(i + 1);
-
-    float* img_x_1 = img_x.ptr<float>(i);
-    float* img_y_1 = img_y.ptr<float>(i);
-
-    for (int j = 1; j < img.cols-1; ++j) {
-      img_x_1[j] = - img_0[j - 1] + img_0[j + 1] - 2.0 * img_1[j - 1] +
-          2.0 * img_1[j + 1] - img_2[j - 1] + img_2[j + 1];
-
-      img_y_1[j] = - img_0[j - 1] - 2.0 * img_0[j] - img_0[j + 1] +
-          img_2[j - 1] + 2.0 * img_2[j] + img_2[j + 1];
-    }
-  }
-}
-
-void gradientSobel_zeropadding(cv::Mat& img, cv::Mat& img_x, cv::Mat& img_y) {
-  // gradiente en X
   assert((img.rows == img_x.rows) && (img.cols == img_x.cols) && \
          (img.cols == img_y.cols) && (img.cols == img_y.cols));
 
@@ -49,13 +19,9 @@ void gradientSobel_zeropadding(cv::Mat& img, cv::Mat& img_x, cv::Mat& img_y) {
   // - implementar "zero padding" en el c贸mputo del gradiente
   // - analizar correlacion vs. convoluci贸n
   cv::Mat padding(img.rows, img.cols, CV_32FC1);
-  copyMakeBorder(img, padding, 1, 1, 1, 1, BORDER_CONSTANT, cv::Scalar(0));
+  copyMakeBorder(img, padding, 1, 1, 1, 1, IPL_BORDER_CONSTANT, cv::Scalar(0));
 
-  // EJERCICIO 3
-  // - c贸mputo usando m谩scaras separables
-  // * implementar como kernel en GPU
-
-  for (int i = 0; i < img.rows; ++i) {
+  for (int i = 1; i < padding.rows-1; ++i) {
     float* img_0 = padding.ptr<float>(i - 1);
     float* img_1 = padding.ptr<float>(i);
     float* img_2 = padding.ptr<float>(i + 1);
@@ -63,7 +29,7 @@ void gradientSobel_zeropadding(cv::Mat& img, cv::Mat& img_x, cv::Mat& img_y) {
     float* img_x_1 = img_x.ptr<float>(i);
     float* img_y_1 = img_y.ptr<float>(i);
 
-    for (int j = 0; j < img.cols; ++j) {
+    for (int j = 1; j < padding.cols-1; ++j) {
       img_x_1[j] = - img_0[j - 1] + img_0[j + 1] - 2.0 * img_1[j - 1] +
           2.0 * img_1[j + 1] - img_2[j - 1] + img_2[j + 1];
 
@@ -72,6 +38,56 @@ void gradientSobel_zeropadding(cv::Mat& img, cv::Mat& img_x, cv::Mat& img_y) {
     }
   }
 }
+
+
+void gradientSobel_2masks(cv::Mat& img, cv::Mat& img_x, cv::Mat& img_y) {
+  assert((img.rows == img_x.rows) && (img.cols == img_x.cols) && \
+         (img.cols == img_y.cols) && (img.cols == img_y.cols));
+
+  cv::Mat padding(img.rows, img.cols, CV_32FC1);
+  copyMakeBorder(img, padding, 1, 1, 1, 1, IPL_BORDER_CONSTANT, cv::Scalar(0));
+
+  cv::Mat aux_x(padding.rows, padding.cols, CV_32FC1);
+  cv::Mat aux_y(padding.rows, padding.cols, CV_32FC1);
+  // EJERCICIO 3
+  // - computo usando mascaras separables
+  // La idea es implementar el fitro de Sobel
+  // como dos operaciones de filtros
+  // unidimensionales (reducir cantidad de
+  // computos y cache misses).
+
+  for (int i = 1; i < padding.rows-1; ++i) {
+    float* img_0 = padding.ptr<float>(i - 1);
+    float* img_1 = padding.ptr<float>(i);
+    float* img_2 = padding.ptr<float>(i + 1);
+
+    float* aux_x_1 = aux_x.ptr<float>(i);
+    float* aux_y_1 = aux_y.ptr<float>(i);
+
+    for (int j = 1; j < padding.cols-1; ++j) {
+      aux_x_1[j] = - img_1[j - 1] + img_1[j + 1]; 
+      aux_y_1[j] = img_1[j - 1] + 2.0 * img_1[j] + img_1[j + 1];
+    }
+  }
+  for (int i = 1; i < padding.rows-1; ++i) {
+    float* img_0_x = aux_x.ptr<float>(i - 1);
+    float* img_1_x = aux_x.ptr<float>(i);
+    float* img_2_x = aux_x.ptr<float>(i + 1);
+    float* img_0_y = aux_y.ptr<float>(i - 1);
+    float* img_1_y = aux_y.ptr<float>(i);
+    float* img_2_y = aux_y.ptr<float>(i + 1);
+
+    float* img_x_1 = img_x.ptr<float>(i-1);
+    float* img_y_1 = img_y.ptr<float>(i-1);
+
+    for (int j = 1; j < padding.cols-1; ++j) {
+      img_x_1[j-1] = img_0_x[j] + 2.0 * img_1_x[j] + img_2_x[j]; 
+      img_y_1[j-1] = - img_0_y[j] + img_2_y[j + 1];
+    }
+  }
+}
+
+
 void local_maxima_3x3(cv::Mat& img, std::vector<cv::Point>& points) {
   points.clear();
   for (int i = 1; i < img.rows - 1; ++i) {
@@ -121,24 +137,24 @@ int main(int argc, char** argv )
   cv::Mat gray;
   cv::cvtColor(image, gray, CV_BGR2GRAY);
 
-  // Descomentar para sacar mas ruido de la imagen
-  //GaussianBlur(gray, gray, cv::Size(), SIGMA, SIGMA, cv::BORDER_DEFAULT);
+  // sacar un poco mas de ruido
+  //GaussianBlur(gray, gray, cv::Size(), SIGMA1, SIGMA1, cv::BORDER_DEFAULT);
 
-  // uint8 -> float (驴PO RQUE?)
+  // uint8 -> float (驴PR QUE?)
   cv::Mat gray_(image.rows, image.cols, CV_32FC1);
   gray.convertTo(gray_, CV_32F);
 
-  // c贸mputo de derivadas espaciales
+  // co砿puto de derivadas espaciales
   cv::Mat im_x(image.rows, image.cols, CV_32FC1);
   cv::Mat im_y(image.rows, image.cols, CV_32FC1);
-  gradientSobel_zeropadding(gray_, im_x, im_y);
+  gradientSobel_2masks(gray_, im_x, im_y);
   //gradientSobel(gray_, im_x, im_y);
 
   cv::Mat a11 = im_x.mul(im_x);
   cv::Mat a12 = im_x.mul(im_y);
   cv::Mat a22 = im_y.mul(im_y);
 
-  // integraci贸n local = Gaussian blur
+  // integracio硁 oocal = Gaussian blur
   cv::Mat a11w(image.rows, image.cols, CV_32FC1);
   cv::Mat a12w(image.rows, image.cols, CV_32FC1);
   cv::Mat a22w(image.rows, image.cols, CV_32FC1);
@@ -146,7 +162,7 @@ int main(int argc, char** argv )
   GaussianBlur(a12, a12w, cv::Size(), SIGMA, SIGMA, cv::BORDER_DEFAULT);
   GaussianBlur(a22, a22w, cv::Size(), SIGMA, SIGMA, cv::BORDER_DEFAULT);
 
-  // c贸mputo de funci贸n R
+  // c贸mputo de fnnco贸n R
   cv::Mat R = a11w.mul(a22w) - a12w.mul(a12w) - K_ * (a11w + a22w).mul(a11w + a22w);
 
   // umbralizado
@@ -155,6 +171,7 @@ int main(int argc, char** argv )
   // non-maxima supression (NMS)
   std::vector<cv::Point> harris_corners;
   local_maxima_3x3(R, harris_corners);
+  std::cout<<"harris corners size: "<<harris_corners.size()<<std::endl;
 
   // R al rango [0, 1] antes de visualizar
   double min;
@@ -164,7 +181,7 @@ int main(int argc, char** argv )
   R.convertTo(R_, CV_32FC1, 1.0 / (max - min), -min / (max - min));
   cv::minMaxIdx(R_, &min, &max);
 
-  // visualizaci贸n
+  // visualizacio硁
   cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
   cv::imshow("Image", image);
 
